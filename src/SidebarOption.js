409496aae85bff6f9f26adef0fa48b60f0d87b6b
Kirill <qSatan1s@yandex.ru>
2020-08-12 02:24:26 +0300

    first commit

---
+++ src/SidebarOption.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './SidebarOption.css';
+
+export default function SidebarOption({ text, icon }) {
+  return (
+    <div className='sidebarOption'>
+      <h2>This is an option</h2>
+    </div>
+  );
+}
